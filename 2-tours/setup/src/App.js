8e22523fbb7eb99daff252ccb1f8b099e248208f
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-05 01:09:55 +0300

    2nd project- tours fetching data and adding read more button

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -1,11 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    //extra precaustion to set loading to true
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []); //make sure only runs once
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
